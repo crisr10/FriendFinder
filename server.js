6bf126635098543c8cd449e6e5472cd891ce69c5
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-01-11 13:40:06 -0800

    changes in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Create two HTML files.
 
-//The first file will be the index html, this file will be constructed with bootstrap, I will also need to add a jquery link tag to handle all the requests between javascript and html files.
+//The first file will be the home html, this file will be constructed with bootstrap, I will also need to add a jquery link tag to handle all the requests between javascript and html files.
 
-// The second html file will have a form that asks the user questions and stores this questions inside the app.
+// The second html file will have a form that asks the user questions and stores this questions inside the server.
 
 // Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
 
@@ -37,7 +37,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8085;
+var PORT = process.env.PORT || 8085;
 
 // BodyParser makes it possible for our server to interpret data sent to it.
 // The code below is pretty standard.
