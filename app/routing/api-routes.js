88c40489a35ebead1bf27093b030c62de0eb9f31
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-01-11 11:18:22 -0800

    Added JavaScript for modal in the Survey HTML, also finished the logic on api-routes.js

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -9,7 +9,69 @@ var peopleData = require("../data/friends.js");
 
 // ROUTING
 module.exports = function (app) {
+
+  // API GET Requests
+  // Below code handles when users "visit" a page.
   app.get("/api/friends", function(req, res) {
     res.json(peopleData);
   });
-}
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  app.post("/api/friends", function(req, res) {
+    // Convert user scores array of strings to numbers
+    var scoresStringArray = req.body.scores;
+    var scoresNumberArray = [];
+    var scoresCompare = [];
+    var sum = 0;
+
+    compareScores();
+    findMatch();
+
+    // =====================================================
+    // FUNCTIONS
+    // Function to check scores
+    function compareScores() {
+      for (var i=0; i<scoresStringArray.length; i++){
+        scoresNumberArray.push(parseInt(scoresStringArray[i]));
+      }
+
+      var userScores = req.body.scores;
+
+    // Loop through array of friends to compare to users choices
+    for (var j=0; j<peopleData.length; j++){
+
+      // Loop through array of scores from each friend and add the difference.
+      for (var s=0; s<scoresNumberArray.length; s++){
+        sum += Math.abs(peopleData[j].scores[s]-scoresNumberArray[s]);
+      }
+      // Pushing sums to scoresCompareArray.
+      scoresCompare.push(sum);
+      // Setting sum back to 0 for the next friend.
+      sum = 0;
+    }
+  }
+
+  function findMatch(){
+    var lowestDiff = scoresCompare[0];
+    var matchIndex;
+
+    for (var i=0; i<scoresCompare.length; i++){
+      if (scoresCompare[i]<lowestDiff) {
+        lowestDiff= scoresCompare[i];
+      }
+      matchIndex = (scoresCompare.indexOf(lowestDiff));
+    }
+    var match = peopleData[matchIndex];
+    peopleData.push(req.body);
+    res.json(match);
+  }
+});
+};
+
+
+
+
+
+
+
